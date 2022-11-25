eb2cc08abb60b3877bcedcea81b1bd8ddb1748e6
San-Moro <valentinacortese.dsg@gmail.com>
2022-11-25 16:48:00 +0100

    Foreach don't work

--- index.php
+++ index.php
@@ -56,27 +56,27 @@
         <div class="container">
         <!-- stampare in pagina gli $hotels -->
             <?php
-            for($i=0; $i<count($hotels); $i++) {
-                $curr_hotel = $hotels[$i];
+            foreach($hotels as $key => $value) {
+
                 //var_dump($curr_hotel);
             ?>
             <table class="table">
                 <thead class="table-secondary">
                     <tr>
-                        <th scope="col">name</th>
-                        <th scope="col">description</th>
+                        <th scope="col"><?php echo $key; ?></th>
+                        <!-- <th scope="col">description</th>
                         <th scope="col">parking</th>
                         <th scope="col">vote</th>
-                        <th scope="col">distance_to_center</th>
+                        <th scope="col">distance_to_center</th> -->
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <th scope="row"><?php echo $curr_hotel["name"] ?></th>
-                        <td><?php echo $curr_hotel["description"] ?></td>
-                        <td><?php echo $curr_hotel["parking"] ?></td>
-                        <td><?php echo $curr_hotel["vote"] ?></td>
-                        <td><?php echo $curr_hotel["distance_to_center"] ?></td>
+                        <th scope="row"><?php echo $value?></th>
+                        <td><?php echo $value; ?></td>
+                        <td><?php echo $value; ?></td>
+                        <td><?php echo $value; ?></td>
+                        <td><?php echo $value; ?></td>
                     </tr>
                 </tbody>
             </table>
